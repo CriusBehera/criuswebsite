7fbe90e7235a5a4e8e3c69d9e240d872f27fe52b
CriusBehera <95353090+CriusBehera@users.noreply.github.com>
2021-12-21 15:27:16 +0530

    Add files via upload

---
+++ music/music_script.js
@@ -0,0 +1,29 @@
+function Coffen_Dance() {
+    document.getElementById("Codffen_Dance_music").play();
+    document.getElementById("Faded_Alan_Walker_music").pause();
+    document.getElementById("Shape_Of_You_music").pause();
+    window.alert("Playing Coffen Dance");
+}
+
+function Shape_Of_You() {
+    document.getElementById("Shape_Of_You_music").play();
+    document.getElementById("Codffen_Dance_music").pause();
+    document.getElementById("Faded_Alan_Walker_music").pause();
+    window.alert("Playing Shape Of You");
+}
+
+function Faded_Alan_Walker() {
+    document.getElementById("Faded_Alan_Walker_music").play();
+    document.getElementById("Shape_Of_You_music").pause();
+    document.getElementById("Codffen_Dance_music").pause();
+    window.alert("Playing Faded Alan Walker");
+}
+
+function pause_all_music() {
+    document.getElementById("Codffen_Dance_music").pause();
+    document.getElementById("Faded_Alan_Walker_music").pause();
+    document.getElementById("Shape_Of_You_music").pause();
+    window.alert("Pauseing All Music");
+}
+
+// By Crius Behera
